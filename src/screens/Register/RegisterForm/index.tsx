3013844cbc34f2c0a588fc1e8a0cfbc2b2d1e0b5
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2026-03-04 15:32:15 -0300

    feat: adicionando o contexto de auth no registro, para criar a conta

--- src/screens/Register/RegisterForm/index.tsx
+++ src/screens/Register/RegisterForm/index.tsx
@@ -10,6 +10,8 @@ import { AuthButton } from '@/components/AuthButton';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { PublicStackParamsList } from '@/routes/PublicRoutes';
 import { UserRegisterRequestDTO } from '@/interfaces/auth/request/user-register-request-dto';
+import { useAuthContext } from '@/context/auth.context';
+import { useErrorHandler } from '@/shared/hooks/useErrorHandler';
 
 export const RegisterForm = () => {
     
@@ -27,7 +29,18 @@ export const RegisterForm = () => {
     });
 
     const navigation = useNavigation<NavigationProp<PublicStackParamsList>>();
-    
+
+    const { handleRegister } = useAuthContext();
+    const { handleError } = useErrorHandler();
+
+    const onSubmit = async(userData: UserRegisterRequestDTO) => {
+        try {
+            await handleRegister(userData);
+        } catch (error) {
+            handleError(error, 'Erro ao registrar. Verifique se os dados estão corretos e tente novamente.')
+        }
+    }
+
     return (
         <SafeAreaView className="bg-gray-700 flex-1" edges={['top']}>
             <AuthHeader />
@@ -62,7 +75,7 @@ export const RegisterForm = () => {
                     />
                 </View>
 
-                <AuthButton className='my-8'>
+                <AuthButton className='my-8' onPress={handleSubmit(onSubmit)} disabled={isSubmitting}>
                     Cadastrar
                 </AuthButton>
 
