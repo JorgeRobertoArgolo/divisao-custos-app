790ca646e5cb828c7082bf3b17a5657bfe19ee07
Jorge Roberto Argolo <20232ireads0001@gmail.com>
2026-03-05 08:09:25 -0300

    feat: Adicionando o estilo ao bottom tabs

--- src/routes/PrivateRoutes/index.tsx
+++ src/routes/PrivateRoutes/index.tsx
@@ -1,6 +1,8 @@
 import { Atividades } from '@/screens/Atividades';
 import { Participantes } from '@/screens/Participantes';
 import { Resumo } from '@/screens/Resumo';
+import { colors } from '@/shared/colors';
+import { MaterialIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export type PrivateBottomTabsParamsList = {
@@ -16,22 +18,58 @@ export const PrivateRoutes = () => {
         <PrivateBottomTabs.Navigator
             screenOptions={{
                 headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: colors.gray[700],
+                    borderTopWidth: 1,
+                    borderTopColor: colors.gray[800],
+                    elevation: 0,
+                    paddingVertical: 20,
+                    paddingHorizontal: 24,
+                    marginBottom: 24
+                },
+                tabBarActiveTintColor: colors['green-base'],
+                tabBarInactiveTintColor: colors.gray[400],
+
+                tabBarLabelStyle: {
+                    fontFamily: 'Inter',
+                    fontSize: 14,
+                    fontWeight: '500',
+                    paddingTop: 3,
+                },
+                tabBarItemStyle: {
+                    paddingTop: 4,
+                },
             }}    
             initialRouteName='Atividades'
         >
             <PrivateBottomTabs.Screen 
                 name='Atividades'
                 component={Atividades}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialIcons name='list' size={24} color={color}/>
+                    )
+                }}
             />
 
             <PrivateBottomTabs.Screen 
                 name='Resumo'
                 component={Resumo}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialIcons name='insert-chart-outlined' size={24} color={color}/>
+                    )
+                }}
             />
 
             <PrivateBottomTabs.Screen 
                 name='Participantes'
                 component={Participantes}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <MaterialIcons name='people-outline' size={24} color={color}/>
+                    )
+                }}
             />
 
         </PrivateBottomTabs.Navigator>
